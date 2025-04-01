f117def0549697c469ae4857abe53bfc6e1fbb59
yunho <Yunho7058@github.com>
2025-04-01 22:21:43 +0900

    img

--- src/component/Box.js
+++ src/component/Box.js
@@ -5,7 +5,14 @@ const Box = (props) => {
   return (
     <div className="box">
       <h1>{props.title}</h1>
-      <img className="item-img" src={props.item && props.item.img} alt="img" />
+      <img
+        className="item-img"
+        src={
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxrDnvdR5nY3CTEL6EcyoJvJWYjnfm9-QvqQ&s' &&
+          props.item.img
+        }
+        alt="img"
+      />
       <h2>WIN</h2>
     </div>
   );
