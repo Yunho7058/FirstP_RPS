1ab6961005c074195752225db469a505ecc249e6
yunho <Yunho7058@github.com>
2025-04-01 22:29:23 +0900

    img2

--- src/component/Box.js
+++ src/component/Box.js
@@ -5,14 +5,12 @@ const Box = (props) => {
   return (
     <div className="box">
       <h1>{props.title}</h1>
-      <img
-        className="item-img"
-        src={
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxrDnvdR5nY3CTEL6EcyoJvJWYjnfm9-QvqQ&s' &&
-          props.item.img
-        }
-        alt="img"
-      />
+      {props.item ? (
+        <img className="item-img" src={props.item.img} alt="img" />
+      ) : (
+        <div className="div-btn">버튼을 클릭해주세요.</div>
+      )}
+
       <h2>WIN</h2>
     </div>
   );
